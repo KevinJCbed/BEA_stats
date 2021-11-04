ea346d94efc10d0bce15481c172d498de327736a
KevinJCbed <62120157+KevinJCbed@users.noreply.github.com>
2021-11-03 22:12:38 -0400

    Update bea_data.py

--- bea_data.py
+++ bea_data.py
@@ -14,6 +14,16 @@ years = [str(x) for x in range(2015, reference_year+1)]
 df_bop = (pd.read_csv("https://raw.githubusercontent.com/KevinJCbed/BEA_stats/main/bop_trade.csv")
           .dropna()[['principal_trading_partners', "series", 'trade']+years]
           )
+df_bop = df_bop[~(df_bop.principal_trading_partners.str.contains(
+    "European Union") | df_bop.principal_trading_partners.str.contains(
+    "elsewhere"))]
+
+add_selectbox = st.sidebar.selectbox(
+    "Which country?",
+    df_bop.query("series == 'Services trade'")[
+        'principal_trading_partners'].unique().tolist()
+)
+
 df_bop['iso'] = cc.countrycode(
     df_bop.principal_trading_partners, origin="country_name", target="iso3c")
 
@@ -27,15 +37,11 @@ imf = pd.read_csv(
     "https://raw.githubusercontent.com/KevinJCbed/BEA_stats/main/IMFWEOOCT2021.csv", encoding="ISO-8859-1")
 
 
-add_selectbox = st.sidebar.selectbox(
-    "Which country?",
-    df_bop.query("series == 'Services trade'")[
-        'principal_trading_partners'].unique().tolist()
-)
-
 select_iso = cc.countrycode(
     add_selectbox, origin="country_name", target="iso3c")
 
+
+df_bop = df_bop.query("iso == @select_iso")
 
 st.title(f"Canada - {add_selectbox} common stats")
 
@@ -43,18 +49,22 @@ st.title(f"Canada - {add_selectbox} common stats")
 st.title("BOP Trade Statistics, $M")
 
 # Metrics
-# col1, col2, col3 = st.columns(3)
-# col1.metric(
-#     label=f"{reference_year} Goods Export Value",
-#     value= df_bop.query("trade == 'Receipts' & series == 'Goods trade'")[str(reference_year)].values[0],
-#     delta = round((df_bop.query("trade == 'Receipts' & series == 'Goods trade'")[str(reference_year)].values[0]/df_bop.query("trade == 'Receipts' & series == 'Goods trade'")[str(reference_year-1)].values[0]-1)*100,1)
-#     )
+col1, col2, col3 = st.columns(3)
+col1.metric(
+    label=f"{str(reference_year)} Goods Export Value",
+    value=int(df_bop.query("trade == 'Receipts' & series == 'Goods trade'")[
+        str(reference_year)].values[0]),
+    delta=round((df_bop.query("trade == 'Receipts' & series == 'Goods trade'")[
+                str(reference_year)].values[0]/df_bop.query("trade == 'Receipts' & series == 'Goods trade'")[str(reference_year-1)].values[0]-1)*100, 1)
+)
 
-# col1.metric(
-#     label="2020 Goods Import Value",
-#     value= df_bop.query("trade == 'Payments' & series == 'Goods trade'")['2020'].values[0],
-#     delta = round((df_bop.query("trade == 'Payments' & series == 'Goods trade'")['2020'].values[0]/df_bop.query("trade == 'Payments' & series == 'Goods trade'")['2019'].values[0]-1)*100,1)
-#     )
+col1.metric(
+    label=f"{str(reference_year)} Goods Import Value",
+    value=int(df_bop.query("trade == 'Payments' & series == 'Goods trade'")[
+        str(reference_year)].values[0]),
+    delta=round((df_bop.query("trade == 'Payments' & series == 'Goods trade'")[
+                str(reference_year)].values[0]/df_bop.query("trade == 'Payments' & series == 'Goods trade'")[str(reference_year-1)].values[0]-1)*100, 1)
+)
 
 # col2.metric(
 #     label="2020 Services Export Value",
@@ -73,8 +83,8 @@ df_bop.series = pd.Categorical(
 df_bop.trade = pd.Categorical(
     df_bop.trade, ['Receipts', "Payments", "Balances", "Bilateral"])
 st.table(
-    df_bop.query("iso == @select_iso").drop("principal_trading_partners",
-                                            axis=1)[["series", 'trade']+years].sort_values(["series", 'trade']).round()
+    df_bop.drop("principal_trading_partners",
+                axis=1)[["series", 'trade']+years].sort_values(["series", 'trade']).round()
 )
 
 
